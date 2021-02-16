477d668fce85c26decd6a5ea51fc302aa8bc25b4
Gonçalves Antoine <antoine.goncalves-isc.france@ibm.com>
2021-02-16 16:27:03 +0100

    add logic for futur template pokemon page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+
+import { Pokemon } from './pokemon.interface';
+import { POKEMONS } from './mock-pokemon';
 
 @Component({
     selector: 'pokemon-app',
-    template: `<h1>Hello {{name}}</h1>`,
+    template: `<h1>Liste de pokémons</h1>`,
 })
-export class AppComponent  {
-    name = 'Angular';
+export class AppComponent implements OnInit {
+    private pokemons: Pokemon[];
+
+    ngOnInit() {
+        this.pokemons = POKEMONS;
+    }
+
+    selectPokemon (pokemon: Pokemon) {
+        alert('Vous avez cliqué sur ' + pokemon.name);
+    }
 }
